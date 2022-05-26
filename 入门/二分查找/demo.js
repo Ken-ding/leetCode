d376eb9e467d3d6b641fb00631dc942680fa7aa8
dxj <dingxj@connor.net.cn>
2022-05-26 10:20:31 +0800

    feat:二分查找法

---
+++ 入门/二分查找/demo.js
@@ -0,0 +1,15 @@
+var search = function(nums, target) {
+    let l=0,h=nums.length-1;
+    while(l<=h){
+        let i=Math.floor((h-l)/2+l)
+        let mid=nums[i]
+        if(target===mid){
+            return i 
+        }else if(target>mid){
+            l=i+1
+        }else if(target<mid){
+            h=i-1
+        }
+    }
+    return -1
+ };
