5df48c948ee9ba3e7a921d940961cca6a5f452b5
dxj <dingxj@connor.net.cn>
2022-06-27 10:24:50 +0800

    feat:完成图像渲染深度优先遍历

--- 入门/图像渲染/demo.js
+++ 入门/图像渲染/demo.js
@@ -3,21 +3,27 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 // 深度优先遍历
-let image = [[1,1,1],[1,1,0],[1,0,1]],sr = 1, sc = 1, newColor = 2;
+let image = [[0,0,0],[0,0,0]],sr = 1, sc = 1, newColor = 2;
  var floodFill = function(image, sr, sc, newColor) {
-    if (image.length === 0) return []
-    const w = image.length, h = image[0].length, oldColor = image[sr][sc]
-    const d = [[1,0],[0,1],[0,-1],[-1,0]]
-    function dfs(i, j) {
-        if (image[i][j] !== oldColor || image[i][j] === newColor) return
-        image[i][j] = newColor
-        for (const [dx, dy] of d) {
-            const x = dx + i, y = dy + j
-            if (x >= 0 && x < w && y >= 0 && y < h) dfs(x, y)
+    //元素的上右下左相邻的坐标
+    const d=[[-1,0],[0,1],[1,0],[0,-1]],set=new Set(),oldColor=image[sr][sc];
+    function dfs(i,j){
+        if(set.has([i,j].toString()))return;
+        set.add([i,j].toString())
+        if(image[i][j]===oldColor){
+            image[i][j]=newColor;
+        }else{
+            return
+        }
+        for (const [dx,dy] of d) {
+            let x=i+dx,y=j+dy;
+            if(image[x]===(void 0)||image[x][y]===(void 0)){
+                continue
+            }
+            dfs(x,y)
         }
     }
-    dfs(sr, sc)
-    return image
+    dfs(sr,sc)
 };
 
 floodFill(image, sr, sc, newColor)
