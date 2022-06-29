c87a908e799acf102214ef73a04109f5ed79671f
dxj <dingxj@connor.net.cn>
2022-06-29 09:45:17 +0800

    feat:增加广度优先遍历

--- 入门/图像渲染/demo.js
+++ 入门/图像渲染/demo.js
@@ -3,8 +3,8 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 // 深度优先遍历
-let image = [[0,0,0],[0,0,0]],sr = 1, sc = 1, newColor = 2;
- var floodFill = function(image, sr, sc, newColor) {
+let image = [[1,1,1],[1,1,0],[1,0,1]],sr = 1, sc = 1, newColor = 2;
+ var floodFill1 = function(image, sr, sc, newColor) {
     //元素的上右下左相邻的坐标
     const d=[[-1,0],[0,1],[1,0],[0,-1]],set=new Set(),oldColor=image[sr][sc];
     function dfs(i,j){
@@ -26,6 +26,26 @@ let image = [[0,0,0],[0,0,0]],sr = 1, sc = 1, newColor = 2;
     dfs(sr,sc)
 };
 
-floodFill(image, sr, sc, newColor)
+// floodFill1(image, sr, sc, newColor)
 
 //广度优先遍历
+var floodFill = function(image, sr, sc, newColor) {
+    if (image.length === 0) return []
+    const w = image.length, h = image[0].length, oldColor = image[sr][sc]
+    const d = [[1,0],[0,1],[0,-1],[-1,0]]
+    const queue = [[sr, sc]]
+    while (queue.length !== 0) {
+        const [i, j] = queue.shift()
+        if (image[i][j] !== oldColor || image[i][j] === newColor) continue
+        image[i][j] = newColor
+        for (const [dx, dy] of d) {
+            const x = dx + i, y = dy + j
+            if (x >= 0 && x < w && y >= 0 && y < h) {
+                queue.push([x,y])
+            }
+        }
+    }
+    return image
+};
+
+floodFill(image, sr, sc, newColor)
