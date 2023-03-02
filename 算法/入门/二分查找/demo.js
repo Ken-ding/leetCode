4ffb13ab7fb4a1a7d496cd96f0b1307c9f44b6cf
dxj <dingxj@connor.net.cn>
2023-03-02 10:16:02 +0800

    feat:增加算法基础-34

--- 算法/入门/二分查找/demo.js
+++ 算法/入门/二分查找/demo.js
@@ -1,15 +1,19 @@
-var search = function(nums, target) {
-    let l=0,h=nums.length-1;
-    while(l<=h){
-        let i=Math.floor((h-l)/2+l)
-        let mid=nums[i]
-        if(target===mid){
-            return i 
-        }else if(target>mid){
-            l=i+1
-        }else if(target<mid){
-            h=i-1
-        }
+let nums = [3, 4, 7, 8, 9, 10];
+var search = function (nums, target) {
+  let l = 0,
+    h = nums.length - 1;
+  while (l <= h) {
+    let i = Math.floor((h - l) / 2 + l);
+    let mid = nums[i];
+    if (target === mid) {
+      return i;
+    } else if (target > mid) {
+      l = i + 1;
+    } else if (target < mid) {
+      h = i - 1;
     }
-    return -1
- };
+  }
+  return -1;
+};
+
+search(nums, 8);
