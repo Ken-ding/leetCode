ab4347e281d89ca67e0d9b84533160a23d46fa2f
dxj <1073031849@qq.com>
2023-03-11 13:02:16 +0800

    feat:增加算法基础

--- 算法/基础/在排序数组中查找元素的第一个和最后一个位置/demo.js
+++ 算法/基础/在排序数组中查找元素的第一个和最后一个位置/demo.js
@@ -3,4 +3,18 @@
  * @param {number} target
  * @return {number[]}
  */
-var searchRange = function (nums, target) {};
+let nums = [1, 5];
+let target = 5;
+var searchRange = function (nums, target) {
+  let left = 0,
+    right = nums.length - 1;
+  while (left <= right) {
+    if (nums[left] !== target) left++;
+    if (nums[right] !== target) right--;
+    if (nums[left] === target && nums[right] === target) break;
+  }
+  if (left > right) return [-1, -1];
+  return [left, right];
+};
+
+searchRange(nums, target);
