c467686b2c4bcb03d3b4eaefd0edb0777432d766
dxj <dingxj@connor.net.cn>
2023-03-24 10:14:08 +0800

    fix:增加基础旋转数组

--- 算法/基础/在排序数组中查找元素的第一个和最后一个位置/demo.js
+++ 算法/基础/在排序数组中查找元素的第一个和最后一个位置/demo.js
@@ -3,4 +3,56 @@
  * @param {number} target
  * @return {number[]}
  */
-var searchRange = function (nums, target) {};
+let nums = [5, 7, 7, 8, 8, 10];
+let target = 4;
+var searchRange1 = function (nums, target) {
+  let left = 0,
+    right = nums.length - 1;
+  while (left <= right) {
+    if (nums[left] !== target) left++;
+    if (nums[right] !== target) right--;
+    if (nums[left] === target && nums[right] === target) break;
+  }
+  if (left > right) return [-1, -1];
+  return [left, right];
+};
+
+// searchRange1(nums);
+
+var searchRange = function (nums, target) {
+  function binarySearch(nums, target, rule) {
+    let left = 0,
+      right = nums.length - 1;
+    while (left <= right) {
+      let half = Math.floor((left + right) / 2);
+      if (rule) {
+        if (nums[half - 1] < target) {
+          left = half;
+          if (nums[half] === target) {
+            return half;
+          }
+        } else {
+          right = half;
+        }
+      } else {
+        if (nums[half + 1] > target) {
+          right = half;
+          if (nums[half] === target) {
+            return half;
+          }
+        } else {
+          left = half;
+        }
+      }
+    }
+
+    return -1;
+  }
+  let leftIndex = binarySearch(nums, target, true);
+  let rightIndex = binarySearch(nums, target, false);
+  return [leftIndex, rightIndex];
+};
+
+searchRange(nums, 8);
+
+
---
+++ 算法/基础/搜索旋转排序数组/demo.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+let nums = [4,5,6,7,0,1,2], target = 0
+var search = function(nums, target) {
+
+};
