25e64e44843b33d9347c0fcb8bc873b4fe17f4d6
dxj <dingxj@connor.net.cn>
2023-03-25 16:14:03 +0800

    fix:增加旋转数组二分搜索

--- 算法/基础/搜索旋转排序数组/demo.js
+++ 算法/基础/搜索旋转排序数组/demo.js
@@ -3,7 +3,31 @@
  * @param {number} target
  * @return {number}
  */
-let nums = [4,5,6,7,0,1,2], target = 0
-var search = function(nums, target) {
-
+let nums = [1, 3];
+target = 3;
+var search = function (nums, target) {
+  let left = 0,
+    right = nums.length - 1;
+  if (nums.length === 0) return -1;
+  if (nums.length === 1 && nums[0] === target) return 0;
+  while (left <= right) {
+    let half = Math.floor((left + right) / 2);
+    if (nums[half] === target) return half;
+    if (left === right && nums[left] !== target) return -1;
+    if (nums[left] < nums[half]) {
+      if (nums[left] <= target && nums[half] >= target) {
+        right = half;
+      } else {
+        left = half + 1;
+      }
+    } else {
+      if (nums[half + 1] <= target && nums[right] >= target) {
+        left = half + 1;
+      } else {
+        right = half;
+      }
+    }
+  }
 };
+
+search(nums, target);
