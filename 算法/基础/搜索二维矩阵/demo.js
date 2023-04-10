d187bf18cc40d3f6081bf1cd5e0dfa2489cfa0cc
dxj <dingxj@connor.net.cn>
2023-04-10 10:13:45 +0800

    feat:增加寻找旋转排序数组中的最小值

--- 算法/基础/搜索二维矩阵/demo.js
+++ 算法/基础/搜索二维矩阵/demo.js
@@ -3,34 +3,42 @@
  * @param {number} target
  * @return {boolean}
  */
-var matrix = [
-    [1, 3, 5, 7],
-    [10, 11, 16, 20],
-    [23, 30, 34, 60],
-  ],
+var matrix = [[1], [3]],
   target = 3;
 var searchMatrix = function (matrix, target) {
+  if (matrix.length === 0) return false;
   let l = 0,
     r = matrix.length - 1,
     t;
-  while (l <= r) {
-    let h = Math.floor((l + r) / 2);
-    if (matrix[h][0] === target) return h;
-    if (l === r) {
-      t = h;
-      break;
-    }
-    if (matrix[h][0] < target) {
-      l = h + 1;
-    } else {
-      r = h;
+  if (matrix.length === 1) {
+    t = 0;
+  } else {
+    while (l <= r) {
+      let h = Math.floor((l + r) / 2);
+      if (matrix[h][0] === target) return h;
+      if (l === r) {
+        t = h;
+        break;
+      }
+      if (matrix[h][0] < target && h !== 0) {
+        l = h + 1;
+      } else {
+        r = h;
+      }
     }
   }
   let l1 = 0,
     r1 = matrix[t].length - 1;
+  if (matrix[t].length === 1) {
+    if (matrix[t][0] === target) return true;
+    return false;
+  }
   while (l1 <= r1) {
-    let h1 = Math.floor((l + r) / 2);
-    if (matrix[t][h1] === target) return h;
+    let h1 = Math.floor((l1 + r1) / 2);
+    if (matrix[t][h1] === target) return true;
+    if (l1 === r1) {
+      return false;
+    }
     if (matrix[t][h1] < target) {
       l1 = h1 + 1;
     } else {
